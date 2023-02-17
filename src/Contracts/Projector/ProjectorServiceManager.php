fb57ac7195edb63ae4cc2baa874b8e6d623eaca4
steph bug <chronhubgit@gmail.com>
2023-02-17 10:07:46 +0100

    fix(contract): should use accept abstract

--- src/Contracts/Projector/ProjectorServiceManager.php
+++ src/Contracts/Projector/ProjectorServiceManager.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Chronhub\Storm\Contracts\Projector;
 
+use Chronhub\Storm\Projector\ProjectorManagerFactory;
+
 interface ProjectorServiceManager
 {
     public function create(string $name): ProjectorManager;
 
     public function extend(string $name, callable $callback): ProjectorServiceManager;
 
-    public function shouldUse(string $driver, string|callable $factory): ProjectorServiceManager;
+    public function shouldUse(string $driver, string|ProjectorManagerFactory $factory): ProjectorServiceManager;
 }
