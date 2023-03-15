d2e717c557d66e519fad1452ec1e6353fe170b46
steph bug <chronhubgit@gmail.com>
2023-03-15 18:52:52 +0100

    tests

---
+++ tests/Unit/Chronicler/TrackTransactionalStreamTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chronhub\Storm\Tests\Unit\Chronicler;
+
+use Chronhub\Storm\Tests\UnitTestCase;
+use PHPUnit\Framework\Attributes\Test;
+use Chronhub\Storm\Chronicler\TrackStream;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Chronhub\Storm\Chronicler\TransactionalEventDraft;
+use Chronhub\Storm\Chronicler\TrackTransactionalStream;
+
+#[CoversClass(TrackTransactionalStream::class)]
+class TrackTransactionalStreamTest extends UnitTestCase
+{
+    #[Test]
+    public function it_create_new_story(): void
+    {
+        $tracker = new TrackTransactionalStream();
+
+        $this->assertInstanceOf(TrackStream::class, $tracker);
+
+        $draft = $tracker->newStory('foo');
+
+        $this->assertEquals(TransactionalEventDraft::class, $draft::class);
+        $this->assertEquals('foo', $draft->currentEvent());
+        $this->assertTrue($tracker->listeners()->isEmpty());
+    }
+
+    #[Test]
+    public function it_disclose_transactional_story(): void
+    {
+        $tracker = new TrackTransactionalStream();
+
+        $this->assertInstanceOf(TrackStream::class, $tracker);
+
+        $draft = $tracker->newStory('foo');
+
+        $this->assertEquals(TransactionalEventDraft::class, $draft::class);
+        $this->assertEquals('foo', $draft->currentEvent());
+        $this->assertTrue($tracker->listeners()->isEmpty());
+
+        $tracker->watch('foo', function (TransactionalEventDraft $story) {
+            $story->deferred(fn () => [$story->hasTransactionAlreadyStarted(), $story->hasTransactionNotStarted()]);
+        });
+
+        $tracker->disclose($draft);
+
+        $this->assertSame([false, false], $draft->promise());
+    }
+}
