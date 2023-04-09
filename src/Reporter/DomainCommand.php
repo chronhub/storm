144af0fbf5dd16fcbd78682981276c5d2ab311ef
steph bug <chronhubgit@gmail.com>
2023-04-09 14:31:38 +0200

    remove duplication use of HasHeaders trait

--- src/Reporter/DomainCommand.php
+++ src/Reporter/DomainCommand.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 namespace Chronhub\Storm\Reporter;
 
 use Chronhub\Storm\Contracts\Reporter\Reporting;
-use Chronhub\Storm\Message\HasHeaders;
 
 abstract class DomainCommand implements Reporting
 {
-    use HasHeaders;
     use HasDomain;
 
     public function type(): DomainType
