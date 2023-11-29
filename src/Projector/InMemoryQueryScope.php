ce154a67cb047a08ab04f80784f247c95561b245
steph bug <chronhubgit@gmail.com>
2023-11-29 19:16:38 +0100

    wip

--- src/Projector/InMemoryQueryScope.php
+++ src/Projector/InMemoryQueryScope.php
@@ -7,6 +7,7 @@ namespace Chronhub\Storm\Projector;
 use Chronhub\Storm\Contracts\Chronicler\InMemoryQueryFilter;
 use Chronhub\Storm\Contracts\Projector\ProjectionQueryFilter;
 use Chronhub\Storm\Contracts\Projector\ProjectionQueryScope;
+use Chronhub\Storm\Projector\Exceptions\InvalidArgumentException;
 use Chronhub\Storm\Reporter\DomainEvent;
 use Chronhub\Storm\Reporter\ExtractEventHeader;
 
@@ -18,10 +19,17 @@ final class InMemoryQueryScope implements ProjectionQueryScope
         {
             use ExtractEventHeader;
 
+            private int $limit;
+
             private int $counter = 0;
 
-            public function __construct(private readonly int $limit = 500)
+            public function __construct(int $limit = 500)
             {
+                if ($limit < 0) {
+                    throw new InvalidArgumentException('Limit must be greater than 0');
+                }
+
+                $this->limit = $limit === 0 ? PHP_INT_MAX : $limit;
             }
 
             private int $currentPosition = 0;
